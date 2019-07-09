b0236abbecd69c288c9d86d402bf27cfd56336a3
Lorunser <ltray99@gmail.com>
2019-07-09 15:51:22 +0100

    Modify imports to comply with ES6

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+const app = express();
+const port = 3000;
 
 //api
 app.get('/books', async (request, response) => {
