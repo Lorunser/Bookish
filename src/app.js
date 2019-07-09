1cb972929ad253454e129d448495fe47ac23d60b
Lorunser <ltray99@gmail.com>
2019-07-09 15:23:26 +0100

    Set up Babel, Typescript and Express

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//api
+app.get('/books', async (request, response) => {
+    response.json("banter: 'hello'");
+});
+
+//serve frontend directory
+app.use(express.static('frontend'));
+
+//custom routes
+//app.use('/test', express.static('frontend/test.html'));
+//app.use('/timetable', express.static('frontend/timetable.html'));
+//app.use('/history', express.static('frontend/history.html'));
+
+//listen
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
