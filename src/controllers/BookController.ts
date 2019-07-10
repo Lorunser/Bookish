b793bd52a75b213383c855a8de3de7b844bac7ca
Lorunser <ltray99@gmail.com>
2019-07-10 15:32:02 +0100

    Added debugging statements to BookController

---
+++ src/controllers/BookController.ts
@@ -0,0 +1,49 @@
+import DbConnection from "../db/DbConnection";
+import Book from "../models/Book";
+import { Router } from "express";
+
+
+export default class BookController{
+    dbc: DbConnection;
+    router: Router;
+
+    constructor(dbc: DbConnection){
+        this.dbc = dbc;
+        this.router = Router();
+
+        //map routes
+        this.router.get('/', /*passport authentication */ this.getAllBooks.bind(this));
+        this.router.get('/:bookid', /*passport */ this.getBook.bind(this));
+    }
+
+    async getAllBooks(request, response){
+        console.log('GET /books/');
+
+        let queryString: String = `
+            SELECT *
+            FROM Books;
+        `;
+
+        let jsonDbArray = await this.dbc.asyncAll(queryString);
+        let bookArray = jsonDbArray.map((jsonBook) => new Book(jsonBook));
+
+        console.log(bookArray);
+        response.json = bookArray;
+    }
+
+    async getBook(request, response){
+        let bookid = request.params.bookid;
+        console.log('GET /books/' + bookid);
+        
+        let queryString = `
+            SELECT *
+            FROM Books
+            WHERE bookid = ${bookid}; 
+        `;
+
+        let jsonBook = await this.dbc.asyncOneOrNone(queryString);
+        let book = new Book(jsonBook);
+
+        response.json = book;
+    }
+}
