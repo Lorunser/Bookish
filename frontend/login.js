fa73af5fe549f83ca8a2a82d8d2772236ad4ed2d
Josh Cudby <josh.cudby@softwire.com>
2019-07-11 10:13:00 +0100

    modified frontend

--- frontend/login.js
+++ frontend/login.js
@@ -1,8 +1,14 @@
-const axios = require('axios');
-
-function loginSubmit(form){
-    const username = form.username.value;
-    const password = form.password.value;
+function loginSubmit(){
+    document.getElementById("loginForm").submit();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const url = 'http://localhost:3000/login?username=' + username + '&password=' + password;
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('POST', url, true);
+    xhttp.onload = function() {
+        console.log('Got xhttp request')
+    }
+    xhttp.send()
 
     
 }
