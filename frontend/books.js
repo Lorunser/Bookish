69be6fa75c0b4ddcea9afbdcdbc0bc9be159c3de
Lorunser <ltray99@gmail.com>
2019-07-11 16:03:39 +0100

    Work on books client-side query

--- frontend/books.js
+++ frontend/books.js
@@ -1,40 +1,3 @@
-function HTMLify(bus) {
-    var detailsHTML = '<span class="badge badge-secondary badge-pill">' + bus.number + '</span>';
-    detailsHTML += " to " + bus.destination + ", arriving in " ;
-    detailsHTML += '<span class="badge badge-primary badge-pill">' + bus.minutesToArrival.toFixed(0) + ' minutes </span>';
-    return detailsHTML;
-}
-
-function createResultsHTML(busStopArray) {
-    var resultsText = '';
-    
-    for (var busStop of busStopArray) {
-        resultsText += '<div class="col-md-6"><ul class="list-group">';
-        resultsText += '<li class="list-group-item active h3">' + busStop.name + '</li>';
-
-        for (var bus of busStop.nextBuses) {
-            var busDetails = HTMLify(bus);
-            resultsText += '<li class="list-group-item list-group-item-action">' + busDetails +'</li>';
-        }
-        
-        if (busStop.nextBuses.length === 0) {
-            resultsText += '<li class="list-group-item list-group-item-action"><b>No buses found</b></li>'
-        }
-
-        resultsText += '</ul></div><br>';
-    }
-
-    return resultsText;
-}
-
-function change_loc(postcode) {
-    var pre_text  ='<iframe width="600" height="500"  src="https://maps.google.com/maps?q='
-    var post_text = '&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>'
-
-    document.getElementById("gmap_canvas_frame").innerHTML = pre_text + postcode + post_text;
-}
-
-
 function getJson(url) {
     var xhttp = new XMLHttpRequest();
 
@@ -60,7 +23,8 @@ function getJson(url) {
 
 function submitBookId() {
     var bookId = document.forms.bookForm[0].value;
-    var url = '/books/' + bookId;
+    //var url = '/books/' + bookId;
+    var url = '/books';
     console.log(bookId);
     getJson(url);
 }
